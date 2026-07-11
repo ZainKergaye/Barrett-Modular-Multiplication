122d2480274cba30930d251b87000a98f461512b
Zain Kergaye <zain@zkergaye.me>
2026-07-11 12:43:58 -0600

    returned k function

--- hdl/folding/barrett_folding_8bit.v
+++ hdl/folding/barrett_folding_8bit.v
@@ -1,16 +1,16 @@
-// Barrett reduction (improved BMM) for 8-bit inputs.
+// Barrett folding reduction for 8-bit inputs.
 // Computes r = (a*b) mod q for fixed (parameter) q.
 //
 // Algorithm:
 //   k  = ceil(log2(q))
-//   μ  = floor(2^(2k + 3) / q)
+//   μ  = floor(2^(2k) / q)
 //   z  = a*b
-//   m1 = floor(z / 2^(k-2))
+//   m1 = floor(z / 2^k)
 //   m2 = m1 * μ
-//   m3 = floor(m2 / 2^(k + 5))
+//   m3 = floor(m2 / 2^k)
 //   t  = z - m3*q
-//   if t>=q return t-q else t
-module barrett_improved_8bit (
+//   if t >= 2q return t-2q else if t>=q return t-q else t
+module barrett_folding_8bit (
 	input  wire clock,
   input  wire [7:0] a,
   input  wire [7:0] b,
@@ -33,24 +33,28 @@ module barrett_improved_8bit (
     end
   endfunction
 
-	reg [16-1:0] k, Mu, z, m1, m2, m3, t, res;
+	reg [16-1:0] s, qPrime, zPrime, k, Mu, z, m1, m2, m3, t, res;
 
 	always @(posedge clock) begin 
 		// Pre computation phase
 		k = ceil_log2(q); // # of bits in q
-		Mu = (1 << (2*k + 3)) / q; // 2^2k / q I don't think floor matters
+		s = k >> 1; // k / 2
+		qPrime = (1 << (3 * s)) % q; // 2^3s mod q not sure how to do the mod q
+		Mu = (1 << (3*s)) / q; // 2^3s / q
 
 		// Integer multiplication phase 
 		z = a * b;
 		
 		// Reduction steps 
-		m1 = z >> (k - 2);
-		m2 = m1 * Mu;
-		m3 = m2 >> (k + 5);
+		m1 = z >> (1 << (3*s)); // z / (2^3s)
+		m2 = m1 * qPrime; 
+		zPrime = m2 + (z % (1<<(3*s))); // m2 + z mod 2^3s
+		m3 = m2 >> k;
 		t = z - m3 * q;
 
-		// Overflow correction
-    if (t >= q)     res = t - q;
+		// Overflow correctio4
+    if (t >= (2*q))      res = t - 2 * q;
+    else if (t >= q)     res = t - q;
     else                 res = t;
 
 	end
